46b29c9d4cf046bd584ce62713ab0fd76a278a6d
Daniel Gutman <danielgutman87@gmail.com>
2021-01-23 15:17:17 +0200

    connect to mongo from app

--- server/app.js
+++ server/app.js
@@ -2,9 +2,14 @@
 const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const socketIo = require('socket.io');
 const cors = require('cors');
 const movies = require('./routes/movies');
+// const users = require('./routes/users');
+
+const connectionString = "mongodb+srv://daniel:daniel11@cluster0.hyprf.mongodb.net/Filmzer?retryWrites=true&w=majority";
+mongoose.connect(connectionString, {useNewUrlParser: true});
 
 var app=express();
 app.use(cors());  
@@ -12,6 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/movies', movies);
+// app.use('/users', users);
 
 const server=http.createServer(app);
 
