59c2d77574ac392f8a9514cac140bf4f9fc827e1
micyakobi <64555783+micyakobi@users.noreply.github.com>
2021-01-26 23:42:02 +0200

    Add Components

--- client/admin/src/app/app.module.ts
+++ client/admin/src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { MoviesComponent } from './components/movies/movies.component';
+import { ReviewsComponent } from './components/reviews/reviews.component';
+import { UsersComponent } from './components/users/users.component';
+import { StatisticsComponent } from './components/statistics/statistics.component';
 const config: SocketIoConfig = { url: 'http://localhost:8080', options: {} };
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    ReviewsComponent,
+    UsersComponent,
+    StatisticsComponent
   ],
   imports: [
     BrowserModule,
