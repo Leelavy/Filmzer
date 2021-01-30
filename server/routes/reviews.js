51bd092de370e71410c7e8e997d07b6aebef9f29
Daniel Gutman <danielgutman87@gmail.com>
2021-01-30 18:53:46 +0200

    add commentout of html address

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -3,15 +3,18 @@ const reviewController = require('../controllers/reviews');
 var router = express.Router();
 
 
+//http://localhost:8080/reviews/getReviewByParams/:reviewRating/:movieTitle/:userName
 router.route('/getReviewByParams/:reviewRating/:movieTitle/:userName')
     .get(reviewController.getReviewsByTitleRatingUsername)
 
 
+//http://localhost:8080/reviews
 router.route('/')
     .post(reviewController.createReview)
     .get(reviewController.getReviews);
 
 
+//http://localhost:8080/reviews/:id
 router.route('/:id')
     .get(reviewController.getReviewById)
     .delete(reviewController.deleteReview)
